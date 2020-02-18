766d4dd17438ad665fe8948ade1b35425d7fc1b5
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-02-17 17:20:09 -0800

    Implement Queens Functions
    Discard original work and implement findSolution() for queens. This was exactly as according to solutions video, for study and reference.

--- src/solvers.js
+++ src/solvers.js
@@ -22,8 +22,13 @@ var makeEmptyMatrix = function(n) {
   });
 };
 
+//the following functions were solved mathematically and not a recursion check for a general case
+//this will be established according to solution video, as we could not follow our far enough to
+//implement the queens case. It will be a whole new example, but will use the successful checks 
+//we have built in Board.js
+
 window.findNRooksSolution = function(n) {
-  var solution = undefined;
+  var solution = null;
 
   var matrix = makeEmptyMatrix(n);
 
@@ -53,69 +58,128 @@ window.countNRooksSolutions = function(n) {
   return solutionCount;
 };
 
+//________________________________________________________________
+//
+//from solution video, with our original work commented out as officially scrapped
+//________________________________________________________________
+//
+
+
+//this is a queen specific solution since we solved rooks using different methods
+
+window.findSolution = function(row, n, board, callback) {
+  //if all rows exhausted
+  if (row === n) {
+  //incremenent solution count
+    callback(); //replaces need for solutioncount within this scope [see video]
+    //stop
+    return;
+  }
+
+  //iterate over possible decisions
+  for (var i = 0; i < n; i++) {
+    //place a piece
+    board.togglePiece(row, i); //THIS is what we were ignoring in our original solution
+    //recurse into remaining problem
+    if (!board.hasAnyQueensConflicts()) {
+      findSolution(row + 1, n, board, callback);
+    }
+    //unplace a piece
+    board.togglePiece(row, i);
+  }
+};
+
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-
-  var board = new Board(makeEmptyMatrix(n));
-
-  if (n === 0) {
-    return [];
-  }
-
-  if (n === 1) {
-    return [[1]];
-  }
-
-  // if (n === 2 || n === 3) {
-  //   return null;
-  // }
-
-  //!!!
-  //need to use correct syntax and variables/function calls on below
-  //!!!
-
-  var firstRow = board.get(0);
-  //create a function that checks if placed piece has any conflicts on next row
-  var checkElement = function(row) {
-    // iterate over first row
-    for (i = 0; i < row.length; i++) {
-      row[i] = 1;
-      // set space to 1
-      // if queen conflict
-      if (this.hasAnyQueenConflictsOn(row, i)) {
-        // set space back to zero
-        row[i] = 0;
-      } else {
-      // if no queen conflicts
-        if (i + 1 !== n) {
-        //if next row index is not n
-          board.checkElement(i + 1);
-          //call recurs with next row index
-        } else {
-          return board;
-        }
-
-        // else return matrix
-      }
-}
-
-    checkElement(firstRow);
-  };
-
-  // at element, set to 1
-  // if queen conflict is false
-  // iterate over next row
+  
+  var board = new Board({n: n});
+  var solution = board.rows(); //test expects a board return of an empty matrix for 'no solution'
 
 
+  findSolution(0, n, board, function() {
+    //as a result of unwinding, the solution found disappears from memory. We need to save a copy somewhere.
+    solution = _.map(board.rows(), function(row) {
+      return row.slice(); //this returns copy of the valid rows and saves a valid board.
+    });
+  });
 
-  // console.log('Single solution for ' + n + ' queens:', JSON.stringify(board));
-  // return board;
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+
+  var board = new Board({n: n});
+
+  findSolution(0, n, board, function() {
+    solutionCount++;
+  });
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+
+
+
+
+//original work for reference:
+
+// var board = new Board(makeEmptyMatrix(n));
+
+// if (n === 0) {
+//   return [];
+// }
+
+// if (n === 1) {
+//   return [[1]];
+// }
+
+// // if (n === 2 || n === 3) {
+// //   return null;
+// // }
+
+// //!!!
+// //need to use correct syntax and variables/function calls on below
+// //!!!
+
+// var firstRow = board.get(0);
+// //create a function that checks if placed piece has any conflicts on next row
+// var checkElement = function(row) {
+//   // iterate over first row
+//   for (i = 0; i < row.length; i++) {
+//     row[i] = 1;
+//     // set space to 1
+//     // if queen conflict
+//     if (this.hasAnyQueenConflictsOn(row, i)) {
+//       // set space back to zero
+//       row[i] = 0;
+//     } else {
+//     // if no queen conflicts
+//       if (i + 1 !== n) {
+//       //if next row index is not n
+//         board.checkElement(i + 1);
+//         //call recurs with next row index
+//       } else {
+//         return board;
+//       }
+
+//       // else return matrix
+//     }
+//   }
+
+//   checkElement(firstRow);
+// };
+
+// // at element, set to 1
+// // if queen conflict is false
+// // iterate over next row
+
+
+
+// // console.log('Single solution for ' + n + ' queens:', JSON.stringify(board));
+// // return board;
+
+
